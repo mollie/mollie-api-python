f8ef36db6834b17d87ca90cb9dfe84f10603d00d
Thijs Riezebeek <thijs@mollie.com>
2017-01-05 15:14:36 +0100

    Bump version (minor) to 1.2.0 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mollie-api-python',
-    version='1.1.4',
+    version='1.2.0',
     license='BSD',
     packages=find_packages(),
     include_package_data=True,
