311c93c6054d642bfd3f617138fc85a6f114a378
Maikel van der Panne <maikel@mollie.com>
2018-01-26 11:49:19 +0100

    Bumped version number to 1.3.0 for release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mollie-api-python',
-    version='1.2.1',
+    version='1.3.0',
     license='BSD',
     packages=find_packages(),
     include_package_data=True,
