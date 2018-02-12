2e45f258fadcd3863292b89d709d41f627f7d3ff
Martijn Smit <smit@mollie.com>
2018-02-12 13:26:15 +0100

    Bump the setup.py version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mollie-api-python',
-    version='1.3.1',
+    version='1.3.2',
     license='BSD',
     packages=find_packages(),
     include_package_data=True,
