27abac9cfe48197296fea69a626d4f7476e7aae9
Tom Hendrikx <tom@fourdigits.nl>
2018-07-18 10:03:22 +0200

    Setup a responses mock, use it in the test

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,37 @@ import os
 
 import pytest
 import Mollie
+import responses
 
 
 @pytest.fixture(scope='session')
 def client():
     """Setup a Mollie API client object."""
-    api_key = os.environ.get('MOLLIE_API_KEY', '')
+    api_key = os.environ.get('MOLLIE_API_KEY', 'test_test')
 
     client = Mollie.API.Client()
     client.setApiKey(api_key)
     return client
+
+
+class ImprovedRequestsMock(responses.RequestsMock):
+    """Wrapper adding a few shorthands to responses.RequestMock."""
+
+    def get(self, url, filename, status=200):
+        """Setup a mock response for a GET request."""
+        file = os.path.join(os.path.dirname(__file__), 'responses', '%s.json' % filename)
+        body = open(file).read()
+        self.add(responses.GET, url, body=body, status=status)
+
+    def post(self, url, filename, status=200):
+        """Setup a mock response for a GET request."""
+        file = os.path.join(os.path.dirname(__file__), 'responses', '%s.json' % filename)
+        body = open(file).read()
+        self.add(responses.POST, url, body=body, status=status)
+
+
+@pytest.fixture
+def response():
+    """Setup the responses fixture."""
+    with ImprovedRequestsMock() as mock:
+        yield mock
--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,11 +1,13 @@
 
-def test_methods_all(client):
+def test_methods_all(client, response):
+    response.get('https://api.mollie.nl/v2/methods', 'methods_ideal')
+
     methods = client.methods.all()
-    assert methods.count > 0
+    assert methods.count == 1
     iterated = 0
     for method in methods:
         iterated += 1
         assert method.id is not None
         assert method.description is not None
         assert method.image_size1x is not None
-    assert iterated == methods.count, 'List count does not match actual iterated methods'
+    assert iterated == 1
