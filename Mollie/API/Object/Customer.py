905d4e9ecfbac9a48bdb8c9451189c0d9d0f873f
Casper Timmers <caspertimmers@MBP-van-Casper.office.fourdigits.nl>
2018-07-18 09:50:15 +0200

    Add customer properties

--- Mollie/API/Object/Customer.py
+++ Mollie/API/Object/Customer.py
@@ -2,4 +2,51 @@ from .Base import Base
 
 
 class Customer(Base):
-    pass
+    @property
+    def id(self):
+        if 'id' not in self:
+            return None
+        return self['id']
+
+    @property
+    def name(self):
+        if 'name' not in self:
+            return None
+        return self['name']
+
+    @property
+    def email(self):
+        if 'email' not in self:
+            return None
+        return self['email']
+
+    @property
+    def locale(self):
+        if 'locale' not in self:
+            return None
+        return self['locale']
+
+    @property
+    def metadata(self):
+        if 'metadata' not in self:
+            return None
+        return self['metadata']
+
+    @property
+    def mode(self):
+        if 'metadata' not in self:
+            return None
+        return self['metadata']
+
+    @property
+    def resource(self):
+        if 'resource' not in self:
+            return None
+        return self['resource']
+
+    @property
+    def createdAt(self):
+        if 'createdAt' not in self:
+            return None
+        return self['createdAt']
+
