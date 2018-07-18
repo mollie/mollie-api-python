1a58725bba600853e404d209cd4ae7938ac6b0fe
Casper Timmers <casper@fourdigits.nl>
2018-07-18 15:09:25 +0200

    remove if statements from Customer.py

--- Mollie/API/Object/Customer.py
+++ Mollie/API/Object/Customer.py
@@ -4,26 +4,18 @@ from .Base import Base
 class Customer(Base):
     @property
     def id(self):
-        if 'id' not in self:
-            return None
         return self.getProperty('id')
 
     @property
     def name(self):
-        if 'name' not in self:
-            return None
         return self.getProperty('name')
 
     @property
     def email(self):
-        if 'email' not in self:
-            return None
         return self.getProperty('email')
 
     @property
     def locale(self):
-        if 'locale' not in self:
-            return None
         return self.getProperty('locale')
 
     @property
@@ -34,19 +26,13 @@ class Customer(Base):
 
     @property
     def mode(self):
-        if 'mode' not in self:
-            return None
         return self.getProperty('mode')
 
     @property
     def resource(self):
-        if 'resource' not in self:
-            return None
         return self.getProperty('resource')
 
     @property
     def createdAt(self):
-        if 'createdAt' not in self:
-            return None
         return self.getProperty('createdAt')
 
