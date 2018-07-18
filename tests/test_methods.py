e55f09f64086dfafd5b9e942caafee2e5cecec85
Tom Hendrikx <tom@fourdigits.nl>
2018-07-18 13:53:53 +0200

    Make test a bit more useful

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,13 +1,16 @@
 
 def test_methods_all(client, response):
-    response.get('https://api.mollie.com/v2/methods', 'methods_ideal')
+    response.get('https://api.mollie.com/v2/methods', 'methods_multiple')
 
     methods = client.methods.all()
-    assert methods.count == 1
+    assert methods.count == 11
     iterated = 0
+    iterated_methods_ids = []
     for method in methods:
         iterated += 1
         assert method.id is not None
+        iterated_methods_ids.append(method.id)
         assert method.description is not None
         assert method.image_size1x is not None
-    assert iterated == 1
+    assert iterated == 11
+    assert len(set(iterated_methods_ids)) == 11, 'Unexpected number of unique methods'
