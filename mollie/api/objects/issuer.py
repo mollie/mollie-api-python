2223b3703fd75205c02ed9731fafc2424bf2923e
Casper Timmers <casper@fourdigits.nl>
2018-07-30 11:59:23 +0200

    add issuer properties

--- mollie/api/objects/issuer.py
+++ mollie/api/objects/issuer.py
@@ -2,4 +2,39 @@ from .base import Base
 
 
 class Issuer(Base):
-    pass
+    @property
+    def resource(self):
+        try:
+            return self._get_property('resource')
+        except KeyError:
+            return None
+
+    @property
+    def id(self):
+        try:
+            return self._get_property('id')
+        except KeyError:
+            return None
+
+    @property
+    def name(self):
+        try:
+            return self._get_property('name')
+        except KeyError:
+            return None
+
+    @property
+    def image_size1x(self):
+        try:
+            images = self._get_property('image')
+            return images['size1x']
+        except KeyError:
+            return None
+
+    @property
+    def image_size2x(self):
+        try:
+            images = self._get_property('image')
+            return images['size2x']
+        except KeyError:
+            return None
