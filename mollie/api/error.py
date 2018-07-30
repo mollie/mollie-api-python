335ce90a3a70bfc6d0c112764604225a5c3ca54e
Tom Hendrikx <tom@fourdigits.nl>
2018-07-30 11:56:03 +0200

    Add specific exceptions so handling exceptions for specific problems is way easier.

--- mollie/api/error.py
+++ mollie/api/error.py
@@ -1,4 +1,18 @@
 class Error(Exception):
+    """Base exception."""
+
     def __init__(self, message=None, field=None):
         Exception.__init__(self, message)
         self.field = field
+
+
+class RequestError(Error):
+    """Errors while preparing or performing an API request."""
+
+    pass
+
+
+class RequestSetupError(RequestError):
+    """Errors related to setting up the request."""
+
+    pass
