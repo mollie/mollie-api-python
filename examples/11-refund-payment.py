3f8383c8b78677749eb91af09b80e4aa2c9f0685
Casper Timmers <casper@fourdigits.nl>
2018-07-31 14:32:35 +0200

    add refund example

---
+++ examples/11-refund-payment.py
@@ -0,0 +1,56 @@
+# coding=utf-8
+#
+# Example 11 - How to prepare a new refund with the Mollie API.
+#
+from __future__ import print_function
+
+import os
+
+import flask
+
+import mollie
+
+
+def main():
+
+
+    #
+    # Initialize the Mollie API library with your API key.
+    #
+    # See: https://www.mollie.com/dashboard/settings/profiles
+    #
+
+    api_key = os.environ.get('MOLLIE_API_KEY', 'test_test')
+    mollie_client = mollie.api.client.Client()
+    mollie_client.set_api_key(api_key)
+
+    body = ''
+    payment_id = ''
+
+    # If no payment ID was provided in the URL, we grab the first payment
+    if payment_id is None:
+        payments = mollie_client.payments.all()
+
+        body += '<p>No payment ID specified. Attempting to retrieve all payments and grabbing the first.</p>'
+
+        if int(payments.count) == 0:
+            body += '<p>You have no payments. You can create one from the examples.</p>'
+            return body
+
+        for payment in payments:
+            payment_id = payment.id
+            break
+
+    payment = mollie_client.payments.get(payment_id)
+    if payment.can_be_refunded and payment.get_amount_remaining['currency'] == 'EUR' and \
+            payment.get_amount_remaining >= 2.0:
+        data = {
+            'amount': {'value': '2.00', 'currency': 'EUR'}
+        }
+        refund = mollie_client.payment_refunds.with_parent_id(payment_id).create(data)
+        body += '<p>%s %s of payment %s refunded</p>' % (refund.amount['currency'], refund.amount['value'],
+                                                         payment_id)
+    else:
+        body += '<p>Payment %s can not be refunded</p>' % payment_id
+    return body
+
