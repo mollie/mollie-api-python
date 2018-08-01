232a9755d6dfbecdab8b36413b1fe30c2e3dc920
Casper Timmers <casper@fourdigits.nl>
2018-08-01 07:24:08 +0200

    remove try except

--- mollie/api/objects/issuer.py
+++ mollie/api/objects/issuer.py
@@ -4,24 +4,15 @@ from .base import Base
 class Issuer(Base):
     @property
     def resource(self):
-        try:
-            return self._get_property('resource')
-        except KeyError:
-            return None
+        return self._get_property('resource')
 
     @property
     def id(self):
-        try:
-            return self._get_property('id')
-        except KeyError:
-            return None
+        return self._get_property('id')
 
     @property
     def name(self):
-        try:
-            return self._get_property('name')
-        except KeyError:
-            return None
+        return self._get_property('name')
 
     @property
     def image_size1x(self):
