58d8110a7834fc7f39009130223e980071f08997
Tom Hendrikx <tom@fourdigits.nl>
2018-08-24 16:34:46 +0200

    Implement links handling for Payment.mandate and Payment.subscription (and Payment.settlement partly)

--- tests/test_payments.py
+++ tests/test_payments.py
@@ -1,14 +1,21 @@
+import pytest
+
 from mollie.api.objects.chargeback import Chargeback
 from mollie.api.objects.customer import Customer
 from mollie.api.objects.list import List
+from mollie.api.objects.mandate import Mandate
 from mollie.api.objects.method import Method
 from mollie.api.objects.payment import Payment
 from mollie.api.objects.refund import Refund
+from mollie.api.objects.subscription import Subscription
 
 PAYMENT_ID = 'tr_7UhSN1zuXS'
 REFUND_ID = 're_4qqhO89gsT'
 CHARGEBACK_ID = 'chb_n9z0tp'
 CUSTOMER_ID = 'cst_8wmqcHMN4U'
+SETTLEMENT_ID = 'stl_jDk30akdN'
+MANDATE_ID = 'mdt_h3gAaD5zP'
+SUBSCRIPTION_ID = 'sub_rVKGtNd6s3'
 
 
 def test_get_all_payments(client, response):
@@ -62,6 +69,10 @@ def test_get_single_payment(client, response):
     response.get('https://api.mollie.com/v2/payments/%s/refunds' % PAYMENT_ID, 'refunds_list')
     response.get('https://api.mollie.com/v2/payments/%s/chargebacks' % PAYMENT_ID, 'chargebacks_list')
     response.get('https://api.mollie.com/v2/customers/%s' % CUSTOMER_ID, 'customer_single')
+    response.get('https://api.mollie.com/v2/customers/{cust}/mandates/{man}'.format(
+        cust=CUSTOMER_ID, man=MANDATE_ID), 'customer_mandate_single')
+    response.get('https://api.mollie.com/v2/customers/{cust}/subscriptions/{sub}'.format(
+        cust=CUSTOMER_ID, sub=SUBSCRIPTION_ID), 'subscription_single')
 
     payment = client.payments.get(PAYMENT_ID)
     assert isinstance(payment, Payment)
@@ -74,7 +85,7 @@ def test_get_single_payment(client, response):
     assert payment.profile_id == 'pfl_QkEhN94Ba'
     assert payment.method == Method.IDEAL
     assert payment.metadata == {'order_id': '12345'}
-    assert payment.sequence_type == Payment.SEQUENCETYPE_ONEOFF
+    assert payment.sequence_type == Payment.SEQUENCETYPE_RECURRING
     assert payment.profile_id == 'pfl_QkEhN94Ba'
     assert payment.checkout_url == 'https://www.mollie.com/payscreen/select-method/7UhSN1zuXS'
     assert payment.resource == 'payment'
@@ -93,6 +104,9 @@ def test_get_single_payment(client, response):
     assert payment.refunds is not None
     assert payment.chargebacks is not None
     assert payment.customer is not None
+    assert payment.settlement is None
+    assert payment.mandate is not None
+    assert payment.subscription is not None
     assert payment.is_open() is True
     assert payment.is_pending() is False
     assert payment.is_canceled() is False
@@ -102,7 +116,7 @@ def test_get_single_payment(client, response):
     assert payment.has_refunds() is True
     assert payment.can_be_refunded() is False
     assert payment.has_sequence_type_first() is False
-    assert payment.has_sequence_type_recurring() is False
+    assert payment.has_sequence_type_recurring() is True
 
 
 def test_payment_get_related_refunds(client, response):
@@ -151,3 +165,38 @@ def test_payment_get_related_customer(client, response):
     customer = payment.customer
     assert isinstance(customer, Customer)
     assert customer.id == CUSTOMER_ID
+
+
+@pytest.mark.xfail(strict=True, reason="Settlement API is not yet implemented")
+def test_payment_get_related_settlement(client, response):
+    """Get the settlement related to the payment."""
+    response.get('https://api.mollie.com/v2/payments/%s' % PAYMENT_ID, 'payment_single')
+    response.get('https://api.mollie.com/v2/settlements/%s' % SETTLEMENT_ID, 'settlement_single')
+
+    payment = client.payments.get(PAYMENT_ID)
+    settlement = payment.settlement
+    # assert isinstance(settlement, Settlement)
+    assert settlement.id == SETTLEMENT_ID
+
+
+def test_payment_get_related_mandate(client, response):
+    """Get the mandate related to the payment."""
+    response.get('https://api.mollie.com/v2/payments/%s' % PAYMENT_ID, 'payment_single')
+    response.get('https://api.mollie.com/v2/customers/{cust}/mandates/{man}'.format(
+        cust=CUSTOMER_ID, man=MANDATE_ID), 'customer_mandate_single')
+
+    payment = client.payments.get(PAYMENT_ID)
+    mandate = payment.mandate
+    assert isinstance(mandate, Mandate)
+    assert mandate.id == MANDATE_ID
+
+
+def test_payment_get_related_subscription(client, response):
+    response.get('https://api.mollie.com/v2/payments/%s' % PAYMENT_ID, 'payment_single')
+    response.get('https://api.mollie.com/v2/customers/{cust}/subscriptions/{sub}'.format(
+        cust=CUSTOMER_ID, sub=SUBSCRIPTION_ID), 'subscription_single')
+
+    payment = client.payments.get(PAYMENT_ID)
+    subscription = payment.subscription
+    assert isinstance(subscription, Subscription)
+    assert subscription.id == SUBSCRIPTION_ID
