4f913039c025ff3a815396b032b33f37b3f15b8f
Tom Hendrikx <tom@fourdigits.nl>
2018-08-24 11:07:24 +0200

    Add the new _get_link() method.

--- mollie/api/objects/base.py
+++ mollie/api/objects/base.py
@@ -5,3 +5,10 @@ class Base(dict):
         if name not in self:
             return None
         return self[name]
+
+    def _get_link(self, name):
+        """Return a link by its name."""
+        try:
+            return self['_links'][name]['href']
+        except KeyError:
+            return None
