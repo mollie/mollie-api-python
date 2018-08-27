a0a1606d115efd3521ac957aa9a39efec60eda8c
Tom Hendrikx <tom@fourdigits.nl>
2018-08-27 15:22:18 +0200

    Test the first item of the list, not the last

--- tests/test_issuers.py
+++ tests/test_issuers.py
@@ -10,10 +10,11 @@ def test_get_issuers(client, response):
     issuers = client.methods.get('ideal', include='issuers').issuers
     assert_list_object(issuers, Issuer)
 
-    # check the last issuer
-    assert issuer.image_svg == 'https://www.mollie.com/external/icons/ideal-issuers/FVLBNL22.svg'
-    assert issuer.image_size1x == 'https://www.mollie.com/images/checkout/v2/ideal-issuer-icons/FVLBNL22.png'
-    assert issuer.image_size2x == 'https://www.mollie.com/images/checkout/v2/ideal-issuer-icons/FVLBNL22.png'
-    assert issuer.name == 'van Lanschot'
+    # check a single retrieved issuer
+    issuer = next(issuers)
+    assert issuer.image_svg == 'https://www.mollie.com/external/icons/ideal-issuers/ABNANL2A.svg'
+    assert issuer.image_size1x == 'https://www.mollie.com/images/checkout/v2/ideal-issuer-icons/ABNANL2A.png'
+    assert issuer.image_size2x == 'https://www.mollie.com/images/checkout/v2/ideal-issuer-icons/ABNANL2A%402x.png'
+    assert issuer.name == 'ABN AMRO'
     assert issuer.resource == 'issuer'
-    assert issuer.id == 'ideal_FVLBNL22'
+    assert issuer.id == 'ideal_ABNANL2A'
