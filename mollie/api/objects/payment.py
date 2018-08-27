7974b4648368665b87bb22d592849d1ecd5894f2
Tom Hendrikx <tom@fourdigits.nl>
2018-08-27 10:11:35 +0200

    Reorder all Payment properties to match the documentation, added missing properties
    see: https://docs.mollie.com/reference/v2/payments-api/get-payment

--- mollie/api/objects/payment.py
+++ mollie/api/objects/payment.py
@@ -14,39 +14,7 @@ class Payment(Base):
     SEQUENCETYPE_FIRST = 'first'
     SEQUENCETYPE_RECURRING = 'recurring'
 
-    def is_open(self):
-        return self._get_property('status') == self.STATUS_OPEN
-
-    def is_pending(self):
-        return self._get_property('status') == self.STATUS_PENDING
-
-    def is_canceled(self):
-        return self._get_property('status') == self.STATUS_CANCELED
-
-    def is_expired(self):
-        return self._get_property('status') == self.STATUS_EXPIRED
-
-    def is_paid(self):
-        return self._get_property('paidAt') is not None
-
-    def is_failed(self):
-        return self._get_property('status') == self.STATUS_FAILED
-
-    def has_refunds(self):
-        return self._get_link('refunds') is not None
-
-    def can_be_refunded(self):
-        return self._get_property('amountRemaining') is not None
-
-    def has_sequence_type_first(self):
-        return self._get_property('sequenceType') == self.SEQUENCETYPE_FIRST
-
-    def has_sequence_type_recurring(self):
-        return self._get_property('sequenceType') == self.SEQUENCETYPE_RECURRING
-
-    @property
-    def checkout_url(self):
-        return self._get_link('checkout')
+    # Documented properties
 
     @property
     def resource(self):
@@ -97,16 +65,16 @@ class Payment(Base):
         return self._get_property('amount')
 
     @property
-    def details(self):
-        return self._get_property('details')
+    def amount_refunded(self):
+        return self._get_property('amountRefunded')
 
     @property
-    def profile_id(self):
-        return self._get_property('profileId')
+    def amount_remaining(self):
+        return self._get_property('amountRemaining')
 
     @property
-    def sequence_type(self):
-        return self._get_property('sequenceType')
+    def description(self):
+        return self._get_property('description')
 
     @property
     def redirect_url(self):
@@ -117,32 +85,66 @@ class Payment(Base):
         return self._get_property('webhookUrl')
 
     @property
-    def description(self):
-        return self._get_property('description')
+    def method(self):
+        return self._get_property('method')
 
     @property
     def metadata(self):
         return self._get_property('metadata')
 
     @property
+    def locale(self):
+        return self._get_property('locale')
+
+    @property
+    def country_code(self):
+        return self._get_property('countryCode')
+
+    @property
+    def profile_id(self):
+        return self._get_property('profileId')
+
+    @property
     def settlement_amount(self):
         return self._get_property('settlementAmount')
 
     @property
-    def method(self):
-        return self._get_property('method')
+    def settlement_id(self):
+        return self._get_property('settlementId')
 
     @property
     def customer_id(self):
         return self._get_property('customerId')
 
     @property
-    def amount_refunded(self):
-        return self._get_property('amountRefunded')
+    def sequence_type(self):
+        return self._get_property('sequenceType')
 
     @property
-    def amount_remaining(self):
-        return self._get_property('amountRemaining')
+    def mandate_id(self):
+        return self._get_property('mandateId')
+
+    @property
+    def subscription_id(self):
+        return self._get_property('subscriptionId')
+
+    @property
+    def order_id(self):
+        return self._get_property('orderId')
+
+    @property
+    def application_fee(self):
+        return self._get_property('applicationFee')
+
+    @property
+    def details(self):
+        return self._get_property('details')
+
+    # documented _links
+
+    @property
+    def checkout_url(self):
+        return self._get_link('checkout')
 
     @property
     def refunds(self):
@@ -163,18 +165,9 @@ class Payment(Base):
             return List(resp, Chargeback)
 
     @property
-    def customer(self):
-        """Return the customer for this payment."""
-        from .customer import Customer
-        url = self._get_link('customer')
-        if url:
-            resp = self._resource.perform_api_call(self._resource.REST_READ, url)
-            return Customer(resp)
-
-    @property
     def settlement(self):
         """
-        Return the settlement for this payment (if any).
+        Return the settlement for this payment.
 
         TODO: Before we can return a Settlement object, we need to implement the Settlement API.
         """
@@ -185,7 +178,7 @@ class Payment(Base):
 
     @property
     def mandate(self):
-        """Return the customer for this payment."""
+        """Return the mandate for this payment."""
         from .mandate import Mandate
         url = self._get_link('mandate')
         if url:
@@ -194,9 +187,62 @@ class Payment(Base):
 
     @property
     def subscription(self):
-        """Return the customer for this payment."""
+        """Return the subscription for this payment."""
         from .subscription import Subscription
         url = self._get_link('subscription')
         if url:
             resp = self._resource.perform_api_call(self._resource.REST_READ, url)
             return Subscription(resp)
+
+    @property
+    def customer(self):
+        """Return the customer for this payment."""
+        from .customer import Customer
+        url = self._get_link('customer')
+        if url:
+            resp = self._resource.perform_api_call(self._resource.REST_READ, url)
+            return Customer(resp)
+
+    @property
+    def order(self):
+        """
+        Return the order for this payment.
+
+        TODO: Before we can return an Order object, we need to implement the Orders API.
+        """
+        url = self._get_link('order')
+        if url:
+            resp = self._resource.perform_api_call(self._resource.REST_READ, url)
+            return resp
+
+    # additional methods
+
+    def is_open(self):
+        return self._get_property('status') == self.STATUS_OPEN
+
+    def is_pending(self):
+        return self._get_property('status') == self.STATUS_PENDING
+
+    def is_canceled(self):
+        return self._get_property('status') == self.STATUS_CANCELED
+
+    def is_expired(self):
+        return self._get_property('status') == self.STATUS_EXPIRED
+
+    def is_failed(self):
+        return self._get_property('status') == self.STATUS_FAILED
+
+    def is_paid(self):
+        return self._get_property('paidAt') is not None
+
+    def has_refunds(self):
+        return self._get_link('refunds') is not None
+
+    def can_be_refunded(self):
+        return self._get_property('amountRemaining') is not None
+
+    def has_sequence_type_first(self):
+        return self._get_property('sequenceType') == self.SEQUENCETYPE_FIRST
+
+    def has_sequence_type_recurring(self):
+        return self._get_property('sequenceType') == self.SEQUENCETYPE_RECURRING
