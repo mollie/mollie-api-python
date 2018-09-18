6216f8d1b42323b8c65df41699c8d10fb58ba4f0
Casper Timmers <casper@fourdigits.nl>
2018-09-18 09:52:22 +0200

    Test the create order function

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -75,3 +75,74 @@ def test_create_order_refund(client, response):
     order = client.orders.get(ORDER_ID)
     refund = order.create_refund(data)
     assert isinstance(refund, Refund)
+
+
+def test_create_order(client, response):
+    """Create an order"""
+    response.post('https://api.mollie.com/v2/orders', 'order_single')
+    data = {
+        'amount': {
+            'value': '1027.99',
+            'currency': 'EUR'
+        },
+        'billingAddress': {
+            'streetAndNumber': 'Keizersgracht 313',
+            'city': 'Amsterdam',
+            'region': 'Noord-Holland',
+            'postalCode': '1234AB',
+            'country': 'NL',
+            'givenName': 'Piet',
+            'familyName': 'Mondriaan',
+            'email': 'piet@mondriaan.com',
+        },
+        'shippingAddress': {
+            'streetAndNumber': 'Prinsengracht 313',
+            'city': 'Haarlem',
+            'region': 'Noord-Holland',
+            'postalCode': '5678AB',
+            'country': 'NL',
+            'givenName': 'Chuck',
+            'familyName': 'Norris',
+            'email': 'norris@chucknorrisfacts.net'
+        },
+        'metadata': {
+            'order_id': '1337',
+            'description': 'Lego cars'
+        },
+        'consumerDateOfBirth': '1958-01-31',
+        'locale': 'nl_NL',
+        'orderNumber': '1337',
+        'redirectUrl': 'https://example.org/redirect',
+        'webhookUrl': 'https://example.org/webhook',
+        'method': 'klarnapaylater',
+        'lines': [
+            {
+                'type': 'physical',
+                'sku': '5702016116977',
+                'name': 'LEGO 42083 Bugatti Chiron',
+                'productUrl': 'https://shop.lego.com/nl-NL/Bugatti-Chiron-42083',
+                'imageUrl': 'https://sh-s7-live-s.legocdn.com/is/image//LEGO/42083_alt1?$main$',
+                'quantity': 1,
+                'vatRate': '21.00',
+                'unitPrice': {
+                    'currency': 'EUR',
+                    'value': '399.00'
+                },
+                'totalAmount': {
+                    'currency': 'EUR',
+                    'value': '698.00'
+                },
+                'discountAmount': {
+                    'currency': 'EUR',
+                    'value': '100.00'
+                },
+                'vatAmount': {
+                    'currency': 'EUR',
+                    'value': '121.14'
+                }
+            },
+
+        ]
+    }
+    order = client.orders.create(data)
+    assert order.id == ORDER_ID
