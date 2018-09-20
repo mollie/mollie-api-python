b338aa995d5baba3ea2755712b6b343ded76b90e
Casper Timmers <casper@fourdigits.nl>
2018-09-20 13:15:25 +0200

    add lines and order properties to the shipment object

--- mollie/api/objects/shipment.py
+++ mollie/api/objects/shipment.py
@@ -1,7 +1,6 @@
 from .base import Base
 from .list import List
 from .order_line import OrderLine
-from .order import Order
 
 
 class Shipment(Base):
@@ -33,8 +32,19 @@ class Shipment(Base):
     def tracking(self):
         return self._get_property('tracking')
 
+    def has_tracking(self):
+        return self.tracking is not None
+
+    def has_tracking_url(self):
+        return self.has_tracking() and self.tracking['url'] is not None
+
+    @property
+    def tracking_url(self):
+        return self.tracking['url'] if self.has_tracking_url() else None
+
     @property
     def lines(self):
+        """Return the order lines of this shipment."""
         lines = self._get_property('lines') or []
         result = {
             '_embedded': {
@@ -47,16 +57,8 @@ class Shipment(Base):
     @property
     def order(self):
         """Return the order of this shipment."""
+        from .order import Order
         url = self._get_link('order')
         if url:
             resp = self._resource.perform_api_call(self._resource.REST_READ, url)
             return Order(resp)
-
-    def has_tracking(self):
-        return self.tracking is not None
-
-    def has_tracking_url(self):
-        return self.has_tracking() and self.tracking['url'] is not None
-
-    def tracking_url(self):
-        return self._get_property('tracking', 'url')
