ae3fd0d6ba1cba243935ed20560691cf176c9f72
Casper Timmers <casper@fourdigits.nl>
2018-09-24 16:12:07 +0200

    catch TypeError in _get_link when return value is None or null in json response

--- mollie/api/objects/base.py
+++ mollie/api/objects/base.py
@@ -19,7 +19,7 @@ class Base(dict):
         """Return a link by its name."""
         try:
             return self['_links'][name]['href']
-        except KeyError:
+        except (KeyError, TypeError):
             return None
 
     @classmethod
