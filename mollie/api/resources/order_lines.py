d8c94c1a96e79252e4b4a61bcbfd01af61c51dc4
Tom Hendrikx <tom@fourdigits.nl>
2018-10-05 13:34:42 +0200

    Add docstring.

--- mollie/api/resources/order_lines.py
+++ mollie/api/resources/order_lines.py
@@ -5,6 +5,9 @@ from .base import Base
 class OrderLines(Base):
     order_id = None
 
+    def get_resource_name(self):
+        return 'orders/{order_id}/lines'.format(order_id=self.order_id)
+
     def get_resource_object(self, result):
         return OrderLine(result, client=self.client)
 
@@ -12,14 +15,16 @@ class OrderLines(Base):
         self.order_id = order_id
         return self
 
-    def get_resource_name(self):
-        return 'orders/{order_id}/lines'.format(order_id=self.order_id)
-
     def on(self, order):
         return self.with_parent_id(order.id)
 
     def delete(self, data):
-        # Delete function on the parent ID without own id
+        """
+        Custom handling for deleting orderlines.
+
+        Orderlines are deleted by issueing a DELETE on the orders/*/lines endpoint,
+        with the orderline IDs and quantities in the request body.
+        """
         path = self.get_resource_name()
         result = self.perform_api_call(self.REST_DELETE, path, data=data)
         return self.get_resource_object(result)
