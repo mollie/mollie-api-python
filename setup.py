a3d3459df65ed5ee36a83ec1a3f67cf4df074dc7
Maikel van der Panne <maikel@mollie.com>
2018-10-09 16:47:59 +0200

    Updated the README and set version to 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='mollie-api-python',
-    version='2.0.0a0',
+    version='2.0.0',
     license='BSD',
     packages=find_packages(),
     include_package_data=True,
