9ea245a6dc519abf9aeebd271919cdcff32eaddc
Maikel van der Panne <maikel@mollie.com>
2018-10-25 09:58:00 +0200

    Update version number

--- mollie/api/client.py
+++ mollie/api/client.py
@@ -27,7 +27,7 @@ except ImportError:
 
 
 class Client(object):
-    CLIENT_VERSION = '2.0.1'
+    CLIENT_VERSION = '2.0.2'
     API_ENDPOINT = 'https://api.mollie.com'
     API_VERSION = 'v2'
     UNAME = ' '.join(platform.uname())
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='mollie-api-python',
-    version='2.0.1',
+    version='2.0.2',
     license='BSD',
     packages=find_packages(),
     include_package_data=True,
