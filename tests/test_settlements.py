6e2d35bc2b2287c050b651ad0ed9eb3187097c43
kingel <franklin@fourdigits.nl>
2019-05-29 13:50:55 +0200

    Test periods for data from json

--- tests/test_settlements.py
+++ tests/test_settlements.py
@@ -1,3 +1,5 @@
+import json
+
 from mollie.api.objects.settlement import Settlement
 from tests.utils import assert_list_object
 
@@ -24,28 +26,113 @@ def test_settlement_get(client, response):
     assert settlement.settled_at == '2018-04-06T09:41:44.0Z'
     assert settlement.amount == {'currency': 'EUR', 'value': '39.75'}
     assert settlement.invoice_id == 'inv_FrvewDA3Pr'
+    assert settlement.periods == json.loads("""{
+                                            "2018": {
+        "4": {
+            "revenue": [
+                {
+                    "description": "iDEAL",
+                    "method": "ideal",
+                    "count": 6,
+                    "amountNet": {
+                        "currency": "EUR",
+                        "value": "86.1000"
+                    },
+                    "amountVat": null,
+                    "amountGross": {
+                        "currency": "EUR",
+                        "value": "86.1000"
+                    }
+                },
+                {
+                    "description": "Refunds iDEAL",
+                    "method": "refund",
+                    "count": 2,
+                    "amountNet": {
+                        "currency": "EUR",
+                        "value": "-43.2000"
+                    },
+                    "amountVat": null,
+                    "amountGross": {
+                        "currency": "EUR",
+                        "value": "43.2000"
+                    }
+                }
+            ],
+            "costs": [
+                {
+                    "description": "iDEAL",
+                    "method": "ideal",
+                    "count": 6,
+                    "rate": {
+                        "fixed": {
+                            "currency": "EUR",
+                            "value": "0.3500"
+                        },
+                        "percentage": null
+                    },
+                    "amountNet": {
+                        "currency": "EUR",
+                        "value": "2.1000"
+                    },
+                    "amountVat": {
+                        "currency": "EUR",
+                        "value": "0.4410"
+                    },
+                    "amountGross": {
+                        "currency": "EUR",
+                        "value": "2.5410"
+                    }
+                },
+                {
+                    "description": "Refunds iDEAL",
+                    "method": "refund",
+                    "count": 2,
+                    "rate": {
+                        "fixed": {
+                            "currency": "EUR",
+                            "value": "0.2500"
+                        },
+                        "percentage": null
+                    },
+                    "amountNet": {
+                        "currency": "EUR",
+                        "value": "0.5000"
+                    },
+                    "amountVat": {
+                        "currency": "EUR",
+                        "value": "0.1050"
+                    },
+                    "amountGross": {
+                        "currency": "EUR",
+                        "value": "0.6050"
+                    }
+                }
+            ]
+        }
+    }
+    }"""
+                                            )
 
+    def test_settlement_get_next(client, response):
+        """Retrieve the details of the current settlement that has not yet been paid out."""
+        response.get('https://api.mollie.com/v2/settlements/next', 'settlement_next')
 
-def test_settlement_get_next(client, response):
-    """Retrieve the details of the current settlement that has not yet been paid out."""
-    response.get('https://api.mollie.com/v2/settlements/next', 'settlement_next')
+        settlement = client.settlements.get('next')
 
-    settlement = client.settlements.get('next')
+        assert isinstance(settlement, Settlement)
+        assert settlement.created_at == '2018-04-06T06:00:01.0Z'
+        assert settlement.settled_at is None
+        assert settlement.amount == {'currency': 'EUR', 'value': '39.75'}
 
-    assert isinstance(settlement, Settlement)
-    assert settlement.created_at == '2018-04-06T06:00:01.0Z'
-    assert settlement.settled_at is None
-    assert settlement.amount == {'currency': 'EUR', 'value': '39.75'}
+    def test_settlement_get_open(client, response):
+        """Retrieve the details of the open balance of the organization. """
+        response.get('https://api.mollie.com/v2/settlements/open', 'settlement_open')
 
+        settlement = client.settlements.get('open')
 
-def test_settlement_get_open(client, response):
-    """Retrieve the details of the open balance of the organization. """
-    response.get('https://api.mollie.com/v2/settlements/open', 'settlement_open')
-
-    settlement = client.settlements.get('open')
-
-    assert isinstance(settlement, Settlement)
-    assert settlement.reference is None
-    assert settlement.created_at == '2018-04-06T06:00:01.0Z'
-    assert settlement.settled_at is None
-    assert settlement.amount == {'currency': 'EUR', 'value': '39.75'}
+        assert isinstance(settlement, Settlement)
+        assert settlement.reference is None
+        assert settlement.created_at == '2018-04-06T06:00:01.0Z'
+        assert settlement.settled_at is None
+        assert settlement.amount == {'currency': 'EUR', 'value': '39.75'}
