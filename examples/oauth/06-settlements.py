9daff293444ea2b774d80654b25c8eb14f40ae25
Coen van der Kamp <coen@fourdigits.nl>
2019-06-07 11:35:23 +0200

    Add settlement examples

--- examples/oauth/06-settlements.py
+++ examples/oauth/06-settlements.py
@@ -7,13 +7,22 @@ from mollie.api.error import Error
 
 def main(client):
     try:
-
         body = ''
+
+        # https://docs.mollie.com/reference/v2/settlements-api/list-settlements
+
+        body += '<h1>List settlements</h1>'
+        response = client.settlements.list()
+
+        print(response)
+        body += str(response)
+
+        settlement_id = 'stl_123'  # Your open settlement id here
 
         # https://docs.mollie.com/reference/v2/settlements-api/get-settlement
 
         body += '<h1>Get settlement</h1>'
-        response = client.settlement.get()
+        response = client.settlements.get(settlement_id)
 
         print(response)
         body += str(response)
@@ -21,7 +30,7 @@ def main(client):
         # https://docs.mollie.com/reference/v2/settlements-api/get-next-settlement
 
         body += '<h1>Get next settlement</h1>'
-        response = client.settlement.next()
+        response = client.settlements.get('next')
 
         print(response)
         body += str(response)
@@ -29,15 +38,7 @@ def main(client):
         # https://docs.mollie.com/reference/v2/settlements-api/get-open-settlement
 
         body += '<h1>Get open settlement</h1>'
-        response = client.settlement.open()
-
-        print(response)
-        body += str(response)
-
-        # https://docs.mollie.com/reference/v2/settlements-api/list-settlements
-
-        body += '<h1>List settlements</h1>'
-        response = client.settlement.list()
+        response = client.settlements.get('open')
 
         print(response)
         body += str(response)
@@ -45,7 +46,7 @@ def main(client):
         # https://docs.mollie.com/reference/v2/settlements-api/list-settlement-payments
 
         body += '<h1>List settlement payments</h1>'
-        response = client.settlement.payments.list()
+        response = client.settlement_payments.with_parent_id(settlement_id).list()
 
         print(response)
         body += str(response)
@@ -53,7 +54,7 @@ def main(client):
         # https://docs.mollie.com/reference/v2/settlements-api/list-settlement-refunds
 
         body += '<h1>List settlement refunds</h1>'
-        response = client.settlement.refunds()
+        response = client.settlement_refunds.with_parent_id(settlement_id).list()
 
         print(response)
         body += str(response)
@@ -61,7 +62,7 @@ def main(client):
         # https://docs.mollie.com/reference/v2/settlements-api/list-settlement-chargebacks
 
         body += '<h1>List settlement chargebacks</h1>'
-        response = client.settlement.chargebacks()
+        response = client.settlement_chargebacks.with_parent_id(settlement_id).list()
 
         print(response)
         body += str(response)
@@ -69,7 +70,8 @@ def main(client):
         # https://docs.mollie.com/reference/v2/settlements-api/list-settlement-captures
 
         body += '<h1>List settlement captures</h1>'
-        response = client.settlement.captures()
+        settlement = client.settlements.get(settlement_id)
+        response = client.settlement_captures.on(settlement).list()
 
         print(response)
         body += str(response)
