ffa1dafda7839000d8a31162b7f4c6287204c3d4
Tom Hendrikx <tom@fourdigits.nl>
2019-12-23 15:44:08 +0100

    Don't mess with 3rd-party internals when it's not needed.
    The "REQUESTS_CA_BUNDLE" envvar is a documented feature of requests.

--- tests/test_client.py
+++ tests/test_client.py
@@ -98,10 +98,8 @@ def test_client_broken_cert_bundle(monkeypatch):
     Under circumstances it could be possible that the certifi package is not correctly installed, broken,
     or just plain too old. Connecting to the Mollie API should fail with an error when the certificate
     cannot be verified.
-
-    We monkeypatch requests with a non-existent path at the location where certifi normally sets the correct path.
     """
-    monkeypatch.setattr(requests.adapters, 'DEFAULT_CA_BUNDLE_PATH', '/does/not/exist')
+    monkeypatch.setenv("REQUESTS_CA_BUNDLE", "/does/not/exist")
 
     client = Client()
     client.set_api_key('test_test')
